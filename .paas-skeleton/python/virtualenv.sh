7fe620b6817b267d94bf779da2c222f5d72b9efa
Erik LaBianca <erik.labianca@gmail.com>
2013-11-27 23:43:04 -0500

    ensure empty bootstrap sets VIRTUAL_ENV before starting

--- .paas-skeleton/python/virtualenv.sh
+++ .paas-skeleton/python/virtualenv.sh
@@ -5,6 +5,7 @@ if [[ "x${VIRTUAL_ENV}x" != "xx" ]]; then
     VE_ROOT=${VIRTUAL_ENV}
 else
     VE_ROOT=${PAAS_SKELETON_WORK_DIR}
+    export VIRTUAL_ENV=${PAAS_SKELETON_WORK_DIR}
 fi
 
 # create the virtual environment only if it does not already exist
