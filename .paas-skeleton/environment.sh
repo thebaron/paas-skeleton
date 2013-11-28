2a872d5c214dceb99d81cfdd5c56218df9aa13bf
Erik LaBianca <erik.labianca@gmail.com>
2013-11-27 23:13:39 -0500

    incorporate existing virtual env if it exists

--- .paas-skeleton/environment.sh
+++ .paas-skeleton/environment.sh
@@ -15,17 +15,37 @@ if [[ "x${PROJECT_HOME_DIR}x" == "xx" ]]; then
     exit(1)
 fi
 
-# bring in per-project configurations
-if [ -f "$PROJECT_HOME_DIR/etc/project-environment.sh" ]; then
-    source $PROJECT_HOME_DIR/etc/project-environment.sh
+# figure out a good location for our working environment
+if [[ "x${VIRTUAL_ENV}x" != "xx" ]]; then
+    # a python virtualenv is active, use it
+    PAAS_SKELETON_WORK_DIR=${VIRTUAL_ENV}
+
+elif [[ "x${OPENSHIFT_DATA_DIR}x" != "xx" ]]; then
+    # we are on openshift, use the data dir
+    PAAS_SKELETON_WORK_DIR=${OPENSHIFT_DATA_DIR}
+else
+    # otherwise, work out of a directory
+    PAAS_SKELETON_WORK_DIR=${HOME}/.paas_skeleton/`basename ${PWD}`
+fi
+export PAAS_SKELETON_WORK_DIR
+
+if [ ! -d ${PAAS_SKELETON_WORK_DIR} ]; then
+    echo "Creating skeleton work dir ${PAAS_SKELETON_WORK_DIR}"
+    mkdir -p ${PAAS_SKELETON_WORK_DIR}
 fi
 
 # bring in local configurations from environment.d/*.sh
-if [ -d "$PROJECT_HOME_DIR/etc/environment.d/" ]; then
-    for i in $PROJECT_HOME_DIR/etc/environment.d/*.sh; do
+if [ -d "${PROJECT_HOME_DIR}/etc/environment.d/" ]; then
+    for i in ${PROJECT_HOME_DIR}/etc/environment.d/*.sh; do
       if [ -r "$i" ]; then
         . $i
       fi
     done
     unset i
 fi
+
+# bring in per-project configurations
+if [ -f "${PROJECT_HOME_DIR}/etc/project-environment.sh" ]; then
+    source ${PROJECT_HOME_DIR}/etc/project-environment.sh
+fi
+
--- .paas-skeleton/python/virtualenv.sh
+++ .paas-skeleton/python/virtualenv.sh
@@ -1,23 +1,10 @@
 # .paas-skeleton/python-virtualenv.sh - owned by paas-skeleton.git
 
-# if no VE_PATH has been specified, default it living inside $HOME
-if [[ "x${VE_PATH}x" == "xx" ]]; then
-    export VE_PATH=${HOME}/.ve
-fi
-if [ ! -d ${VE_PATH} ]; then
-    mkdir -p ${VE_PATH}
-fi
-
-# set the actual virtualenv root location
-# TODO: set virtualenv root location via OPENSHIFT
-# TODO: set virtualenv root location via virtualenvwrapper
-if [[ "x${VE_ROOT}x" == "xx" ]]; then
-    export VE_ROOT=$VE_PATH/`basename $PWD`
-fi
-
-if [ ! -d ${VE_ROOT} ]; then
-    echo "Virtual environment root $VE_ROOT missing, creating"
-    mkdir -p ${VE_ROOT}
+# pick up the currently active virtual environment, if defined
+if [[ "x${VIRTUAL_ENV}x" != "xx" ]]; then
+    VE_ROOT=${VIRTUAL_ENV}
+else
+    VE_ROOT=${PAAS_SKELETON_WORK_DIR}
 fi
 
 # create the virtual environment only if it does not already exist
